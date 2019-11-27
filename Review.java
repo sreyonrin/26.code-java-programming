71c5008c8c1e1d50251407c28bb61b2bf6744562
Sreyorn <rinsreyorn9@gmail.com>
2019-11-27 20:47:09 +0700

    code js

---
+++ Review.java
@@ -0,0 +1,82 @@
+package DemoB;
+import java.util.Scanner;
+public class Review {
+	public static void main(String[] args) {
+//		Scanner Project = new Scanner(System.in);
+//		String letter;
+//		System.out.println("Enter your order");
+//		letter = Project.nextLine();
+//		switch(letter) {
+//		case"a":
+//		case"A":
+//			System.out.println("Your order BayChha");
+//			break;
+//		case"b":
+//		case"B":
+//			System.out.println("Your order Ice Tea");
+//			break;
+//		case"c":
+//		case"C":
+//			System.out.println("Your order suop");
+//			break;			
+//		}
+		
+		// show result 0
+		
+//		int number = 1;
+//		int sum = 0;
+//		while (number >= 5 ) {
+//			if(number %2 == 0) {
+//				sum += number;
+//			}
+//			number++;
+//		}
+//		System.out.println("Sum of even number:" + sum);
+		
+		// show result 3 5 7 7
+		
+//		int j = 1;
+//		do {
+//			j++;
+//			if(j %2 != 0) {
+//				System.out.println(j);
+//			}
+//		}while(j <= 6);
+//		System.out.println(j);
+		
+		// show result error
+		
+//		String text = "Happy Quiz Time"; 
+//		String word = text.charAt(j);
+//		System.out.println(j);
+		
+		// code show for count number 
+//		
+//		int[] orn = new int[20];
+//		int count = 0;
+//		String index = "";
+//		for(int i = 0; i < orn.length; i++) {
+//			int random = (int)(Math.random() *11);
+//			orn[i] =  random;
+//			System.out.print(random + " ");
+//			if(orn[i] == 5) {
+//				index = index + i;
+//				count++;
+//			}
+//			
+//		}
+//		System.out.println(" ");
+//		if(count != 0) {
+//			System.out.println("have 5 :" + " " + count);
+//			System.out.println("index " + "  " + index);
+//		}else {
+//		System.out.print("Not found");
+//	}
+	
+		
+		int [] num = {2,5,6,4};
+		for (int i = 0; i< num.length; i++) {
+			System.out.println(num[i] + "");
+		}
+}
+}
